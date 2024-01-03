7c588d6a5d4a4264fb831a709a99197a5c4a92ea
SamAn Sokunpiseth <133001008+sokunpiseth-dgc@users.noreply.github.com>
2024-01-03 10:19:59 +0700

    Update settings.py

--- pygoat/settings.py
+++ pygoat/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-
+#adafruit_io_key=11111111111111111111
 import os
 import django_heroku
 
